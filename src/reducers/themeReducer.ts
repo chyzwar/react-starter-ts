68f0edf0374e5f57a1fa6b12dc87b8808f61460f
chyzwar <schizek.marcin@gmail.com>
2018-02-07 21:36:51 +0000

    update deps

--- src/reducers/themeReducer.ts
+++ src/reducers/themeReducer.ts
@@ -5,7 +5,7 @@ import { ActionTypes } from "../constants/ActionTypes";
 import { Action } from "../types/Action";
 import { Theme } from "material-ui/styles/createMuiTheme";
 
-export function themeReducer(state = BlueTheme, action: Action): Theme {
+export default function themeReducer(state = BlueTheme, action: Action): Theme {
   switch (action.type) {
     case ActionTypes.CHANGE_THEME:
       if (action.name === "teal") {
